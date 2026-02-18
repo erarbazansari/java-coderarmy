bd6fe8d6579ce31875e487069ebcaad9abe82e10
Arbaz Ansari <mail.arbazansari@gmail.com>
2026-02-18 23:21:10 +0530

    feat: Add Java program demonstrating various variable types and accompanying notes.

---
+++ 03-variables/code/variables.java
@@ -0,0 +1,24 @@
+public class variables {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 20;
+        int i = 30000;
+        long l = 999999999999L;
+        float f = 105.5f;
+        double d = 105.5;
+        char c = 'a';
+        boolean bool = true;
+        String str = "Hello";
+        byte num = 0b101;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+        System.out.println(str);
+        System.out.println(num);
+    }
+}
